2a9a7840fc997f82751ef5bb47ac40ef4e80bcb1
Banditov <vito20samsung21@gmail.com>
2026-09-08 07:58:58 +0700

    wip: Creating absent page view

--- resources/views/livewire/agenda/index.blade.php
+++ resources/views/livewire/agenda/index.blade.php
@@ -1,5 +1,4 @@
-<div 
-    class="glass 4xs:h-[85%] sm:h-[90%] sm:w-full 4xs:w-auto m-15 p-5 rounded-3xl bg-black/20 backdrop-blur-[5px] border border-white/10 shadow-2xl shadow-black/60 4xs:zoom-70 2xs:zoom-75 xs:zoom-80 sm:zoom-70 md:zoom-75 lg:zoom-80 xl:zoom-85 2xl:zoom-85">
+<section>
     <!-- Header -->
     <header class="mb-8 flex items-center justify-between">
         <div class="flex flex-col gap-2 text-white">
@@ -126,4 +125,4 @@
             </div>
         </div>
     </div>
-</div>
+</section>
